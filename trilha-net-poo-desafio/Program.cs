07bf6c40497654d8f1c5e5b8b703ed284af2cab3
Kezia Oliveira <kezia2709@gmail.com>
2025-06-26 20:34:27 -0300

    Desafio resolvido

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,22 @@
+﻿using DesafioPOO.Models;
+
+// DONE: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Teste com o Nokia
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "1111111111111", memoria: 32);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n");
+        
+        // Teste com o Iphone
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 13", imei: "2222222222222", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
